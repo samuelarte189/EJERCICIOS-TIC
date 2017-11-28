5151279f2730efce11541b695b3e9b73f495aa6b
samuelarte189 <33655408+samuelarte189@users.noreply.github.com>
2017-11-28 11:51:42 +0100

    Add files via upload

---
+++ matriz.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int matriz[3][3];
+    int fila,col;
+    int salir;
+    int end1;
+    for(fila=0;fila<3;fila++){
+      for(col=0;col<3;col++){
+        cout<<"matriz["<<fila<<"][";
+        cout<<col<<"]";
+        cin>>matriz[fila][col];
+        
+        }
+        }
+         cout<<end1<<"Hemos acabado";
+         cin>>salir;
+         return 0;
+         }                 
+    
+    
+    
+    
+      
