19d564737095dc18e3dae37ae2d99b2464369e5d
samuelarte189 <33655408+samuelarte189@users.noreply.github.com>
2017-11-30 11:45:11 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int vector1[5];
+    int vector2[5];
+    int vectort[5];
+    int cont;
+    int salir;
+    //rellenamos los vectores
+    for(cont=0;cont<5;cont++){
+    cout<<"vector1["<<cont<<"]=";
+    cin>>vector1[cont];
+    }
+    cout<<"presiona para salir y luego ENTER";
+    cin>>salir;
+    return 0;
+}
+    
+    
